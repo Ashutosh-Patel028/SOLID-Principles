3d465ad5b86fb64088c4ac9ffa84a1a58e8cc628
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-29 11:47:32 +0530

    added an interface Shape: maintains open closed principles

--- src/Cube.java
+++ src/Cube.java
@@ -1,3 +1,7 @@
-public class Cube {
+public class Cube implements Shape{
     
+    @Override
+    public double area(){
+        return 100;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ public class Main {
         AreaCalculator areaCalculator = new AreaCalculator();
         Circle circle = new Circle(10);
         Square square = new Square(10);
+        Cube cube = new Cube();
         ShapesPrinter printer = new ShapesPrinter();
-        List<Object> shapes = List.of(circle, square);
+        List<Shape> shapes = List.of(circle, square,cube);
         int sum = areaCalculator.sum(shapes);
         System.out.println(printer.json(sum));
         System.out.println(printer.csv(sum));
