b5ebc9bd2ea86ad119ac9198d0cbb8c5159215d7
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-29 11:37:35 +0530

    added dedicated class for formatting sum maintainting single respinsibility

---
+++ src/ShapesPrinter.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class ShapesPrinter {
+    
+    public String json(int sum){
+        return "{shapesSum: %s}".formatted(sum);
+    }
+    
+    public String csv(int sum){
+        return "{shapes_sum,%s}".formatted(sum);
+    }
+}
