830acf6fc4a6bd62a6c50bb7f2756cb05002bd11
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-29 10:40:36 +0530

    modified areaCalculator

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -5,13 +5,14 @@ public class AreaCalculator {
     public int sum(List<Object> shapes){
         int sum=0;
         for(int i=0;i<shapes.size();i++){
-            Object Shape = shapes.get(i);
-            if(Shape instanceof Square){
+            Object shape = shapes.get(i);
+            if(shape instanceof Square){
                 sum+=Math.pow(((Square) shape).getLength(),2);
             }
             if(shape instanceof Circle){
                 sum+=Math.PI * Math.pow(((Circle) shape).getRadius(), 2);
             }
         }
+        return sum;
     }
 }
