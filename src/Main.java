1620d743adf0e8be99cc25e32dd2e10398b39ba9
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-29 11:53:13 +0530

    added rectangle class: open-closed principle maintains

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,9 @@ public class Main {
         Circle circle = new Circle(10);
         Square square = new Square(10);
         Cube cube = new Cube();
+        Rectangle rectangle = new Rectangle();
         ShapesPrinter printer = new ShapesPrinter();
-        List<Shape> shapes = List.of(circle, square,cube);
+        List<Shape> shapes = List.of(circle,square,cube,rectangle);
         int sum = areaCalculator.sum(shapes);
         System.out.println(printer.json(sum));
         System.out.println(printer.csv(sum));
