af74d39bc396cb4be2466c82af3fa0bcbb7bc997
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-29 11:58:27 +0530

    added noshape class: breaks Liskov Substitution

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,9 @@ public class Main {
         Square square = new Square(10);
         Cube cube = new Cube();
         Rectangle rectangle = new Rectangle();
+        Shape noShape = new NoShape();
         ShapesPrinter printer = new ShapesPrinter();
-        List<Shape> shapes = List.of(circle,square,cube,rectangle);
+        List<Shape> shapes = List.of(circle,square,cube,rectangle,noShape);
         int sum = areaCalculator.sum(shapes);
         System.out.println(printer.json(sum));
         System.out.println(printer.csv(sum));
